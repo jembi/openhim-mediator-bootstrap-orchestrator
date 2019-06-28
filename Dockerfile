20540cf79de0d0fc117e167ce4e5bb00c9c59426
MattyJ007 <matthew.dickie@jembi.org>
2019-06-28 16:30:43 +0200

    Update the orchestrator mediator
    Simplify the code to make it easier to understand for Mediator Framework
    beginners...
    
    OHM-816

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,11 @@
 FROM node:10
 WORKDIR /app
 
-# Install node packages
-COPY package.json /app
-COPY package-lock.json /app
-RUN npm install
-
 # Copy app files into container
 COPY . /app
 
-# Set ENV variables
-ARG openhim_api_url=https://openhim-core:8080
-ENV OPENHIM_API_URL=$openhim_api_url
-ARG openhim_trust_self_signed=false
-ENV OPENHIM_TRUST_SELF_SIGNED=$openhim_trust_self_signed
+# Install node packages
+RUN npm install
 
 CMD npm start
-EXPOSE 3001
+EXPOSE 3000
--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-#!/usr/bin/env node
 'use strict'
 
-const express = require('express')
-const mediatorUtils = require('openhim-mediator-utils')
-const pino = require('pino')
-const request = require('request')
-const uuid = require('uuid/v4')
-const parser = require('xml2json')
-const URL = require('url')
+import express from 'express'
+import {
+  activateHeartbeat,
+  fetchConfig,
+  registerMediator
+} from 'openhim-mediator-utils'
+import { get } from 'request'
+import uuid from 'uuid/v4'
+import { toJson } from 'xml2json'
+import { resolve } from 'url'
 
-const {
-  MEDIATOR_PORT,
-  MEDIATOR_HEARTBEAT,
-  OPENHIM_USERNAME,
-  OPENHIM_PASSWORD,
-  OPENHIM_API_URL,
-  OPENHIM_TRUST_SELF_SIGNED,
-  LOG_LEVEL
-} = require('./constants.js')
-const mediatorConfig = require('./mediatorConfig.json')
+import mediatorConfig, { urn } from './mediatorConfig.json'
+
+const openhimConfig = {
+  username: 'root@openhim.org',
+  password: 'password',
+  apiURL: 'https://openhim-core:8080',
+  trustSelfSigned: true,
+  urn
+}
+
 let config = {}
-
-const logger = pino({
-  level: LOG_LEVEL,
-  prettyPrint: true,
-  serializers: {
-    err: pino.stdSerializers.err
-  }
-})
 
 function buildReturnObject(urn, status, statusCode, responseBody) {
   var response = {
@@ -45,9 +39,9 @@ function buildReturnObject(urn, status, statusCode, responseBody) {
 }
 
 function transformDhisData(xmlData) {
-  let jsonDhisData = parser.toJson(xmlData, { object: true, trim: true })
+  const jsonDhisData = toJson(xmlData, { object: true, trim: true })
 
-  logger.info('Received XML data from DHIS2')
+  console.log('Received XML data from DHIS2')
 
   if (
     jsonDhisData &&
@@ -58,131 +52,95 @@ function transformDhisData(xmlData) {
     const organisationUnits =
       jsonDhisData.metadata.organisationUnits.organisationUnit
 
-    logger.info('Adding universally unique id(uuid) to each org unit...')
+    console.log('Adding universally unique id(uuid) to each org unit...')
     organisationUnits.forEach(organisationUnit => {
       organisationUnit.systemID = uuid()
     })
     return organisationUnits
   } else {
     // The Data structure may change with DHIS2 versions
-    logger.error(`Unexpected data structure: ${jsonDhisData}`)
+    console.error(`Unexpected data structure: ${jsonDhisData}`)
     return null
   }
 }
 
-function setUpApp() {
-  const app = express()
+const app = express()
 
-  app.get('/facilities', async (req, res) => {
-    res.set('Content-Type', 'application/json+openhim')
-    if (config.dhis && config.dhis.url && config.dhis.path) {
-      const dhisUri = URL.resolve(config.dhis.url, config.dhis.path)
-      request.get(dhisUri, (err, resp, body) => {
-        if (err) {
-          logger.error('Failed request to DHIS.', err)
-          res.send(buildReturnObject(mediatorConfig.urn, 'Failed', 500, err))
-          return
-        }
-        if (resp.statusCode !== 200) {
-          logger.error(
-            `Unexpected status code from DHIS: ${
-              resp.statusCode
-            }. URL: ${dhisUri}`
-          )
-          res.send(
-            buildReturnObject(
-              mediatorConfig.urn,
-              'Failed',
-              resp.statusCode,
-              resp.body
-            )
-          )
-          return
-        }
-        let facilities
-
-        try {
-          facilities = transformDhisData(body)
-        } catch (err) {
-          logger.error('Error parsing xml', err.message)
-          res.send(buildReturnObject(mediatorConfig.urn, 'Failed', 500, err))
-          return
-        }
-
-        logger.info('Successfully transformed data from DHIS!')
-        res.send(
-          buildReturnObject(mediatorConfig.urn, 'Successful', 200, facilities)
-        )
-      })
-    } else {
-      logger.error('Missing mediator config...')
-      res.send(
-        buildReturnObject(mediatorConfig.urn, 'Failed', 400, {
-          message: 'Please add DHIS2 config via OpenHIM console'
-        })
-      )
-    }
-  })
-
-  app.all('*', (req, res) => {
-    logger.info('Current config: ', config)
-    res.send('Hope you are enjoying the tutorial!!')
-  })
-  return app
-}
-
-const openHimConfig = {
-  username: OPENHIM_USERNAME,
-  password: OPENHIM_PASSWORD,
-  apiURL: OPENHIM_API_URL,
-  trustSelfSigned: OPENHIM_TRUST_SELF_SIGNED,
-  urn: mediatorConfig.urn
-}
-
-function start(callback) {
-  if (OPENHIM_TRUST_SELF_SIGNED) {
-    logger.warn('Disabled TLS Authentication!!')
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  }
-
-  mediatorUtils.registerMediator(openHimConfig, mediatorConfig, err => {
-    if (err) {
-      logger.error('Failed to register mediator. Check your Config...')
-      logger.error(err)
-      process.exit(1)
-    }
-
-    mediatorUtils.fetchConfig(openHimConfig, (err, newConfig) => {
+app.get('/facilities', async (req, res) => {
+  res.set('Content-Type', 'application/json+openhim')
+  if (config.dhis && config.dhis.url && config.dhis.path) {
+    const dhisUri = resolve(config.dhis.url, config.dhis.path)
+    get(dhisUri, (err, resp, body) => {
       if (err) {
-        logger.error('Failed to fetch initial config')
-        logger.error(err)
-        process.exit(1)
+        console.error('Failed request to DHIS.', err)
+        res.send(buildReturnObject(urn, 'Failed', 500, err))
+        return
+      }
+      if (resp.statusCode !== 200) {
+        console.error(
+          `Unexpected status code from DHIS: ${
+            resp.statusCode
+          }. URL: ${dhisUri}`
+        )
+        res.send(buildReturnObject(urn, 'Failed', resp.statusCode, resp.body))
+        return
+      }
+      let facilities
+
+      try {
+        facilities = transformDhisData(body)
+      } catch (err) {
+        console.error('Error parsing xml', err.message)
+        res.send(buildReturnObject(urn, 'Failed', 500, err))
+        return
       }
 
-      logger.info(`Received new config: ${JSON.stringify(newConfig)}`)
-      config = newConfig
-
-      logger.info('Successfully registered mediator!')
-
-      let app = setUpApp()
-      const server = app.listen(MEDIATOR_PORT, () => {
-        if (MEDIATOR_HEARTBEAT) {
-          const configEmitter = mediatorUtils.activateHeartbeat(openHimConfig)
-          configEmitter.on('error', err => {
-            Winston.error('Heartbeat failed', err)
-          })
-
-          configEmitter.on('config', newConfig => {
-            logger.info('Received updated config:', JSON.stringify(newConfig))
-
-            // Update config for mediator
-            config = newConfig
-          })
-        }
-        callback(server)
-      })
+      console.log('Successfully transformed data from DHIS!')
+      res.send(buildReturnObject(urn, 'Successful', 200, facilities))
     })
-  })
-}
+  } else {
+    console.error('Missing mediator config...')
+    res.send(
+      buildReturnObject(urn, 'Failed', 400, {
+        message: 'Please add DHIS2 config via OpenHIM console'
+      })
+    )
+  }
+})
 
-start(() => logger.info(`Listening on port ${MEDIATOR_PORT}...`))
+app.all('*', (req, res) => {
+  res.send('Hope you are enjoying the tutorial!!')
+})
+
+app.listen(3001, () => {
+  const configEmitter = activateHeartbeat(openhimConfig)
+  configEmitter.on('error', err => {
+    console.error('Heartbeat failed', err)
+  })
+
+  configEmitter.on('config', newConfig => {
+    console.log('Received updated config:', JSON.stringify(newConfig))
+
+    // Update config for mediator
+    config = newConfig
+  })
+})
+
+registerMediator(openhimConfig, mediatorConfig, err => {
+  if (err) {
+    console.error('Failed to register mediator. Check your Config: ', err)
+    process.exit(1)
+  }
+
+  console.log('Successfully registered mediator!')
+})
+
+fetchConfig(openhimConfig, (err, initialConfig) => {
+  if (err) {
+    console.error('Failed to fetch initial config: ', err)
+    process.exit(1)
+  }
+
+  console.log('Initial Config: ', JSON.stringify(initialConfig))
+  config = initialConfig
+})
